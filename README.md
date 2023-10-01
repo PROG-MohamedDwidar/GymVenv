407c2d5abf468135e3de022bc0c9c8214f23355c
unknown <dwidar.mohamed@icloud.com>
2023-10-02 00:57:32 +0300

    keras failed

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GymVenv
-Simulation environmet for reinforcement learning
+Simulation environmet for reinforcement learning  
+  
+  
+  
+# to avoid the __version__ error remeber:  
+-In Python310\lib\site-packages\rl\callbacks.py change from tensorflow.keras to from keras import __version__. that should do the trick.  
+-In Python310\lib\site-packages\rl\util.py change optimizer._name to optimizer.name    
+  
+## don't use keras-rl2 it is discontinued and it is the cause of the problems
--- simulation.py
+++ simulation.py
@@ -1,32 +1,60 @@
 import gymnasium as gym
 import random
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation, Flatten
-from tensorflow.keras.optimizers import Adam
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten
+from keras.optimizers import Adam
 
 from rl.agents import DQNAgent
 from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
 
-env = gym.make("CartPole-v1", render_mode="human")
-observation, info = env.reset()
+env = gym.make("LunarLander-v2",render_mode='human')
 
-for _ in range(1000):
-    env.reset()
-      # agent policy that uses the observation and info
-    #action = env.action_space.sample()  # agent policy that uses the observation and info
-    score=0
-    terminated=False
-    truncated=False
-    while not terminated and not truncated:
-        action = random.choice([0, 1])
-        observation, reward, terminated, truncated, info = env.step(action)
-        #print(observation, reward, terminated, truncated, info)
-        score+=reward
-        print(score)
-        env.render()
-    
-    
+# states=env.observation_space.shape[0]
+actions=env.action_space.n
+# print(states)
+print(actions)
+model=Sequential()
+# model.add(Flatten(input_shape=(1,)+states))
+model.add(Flatten(input_shape=(1,)+env.observation_space.shape))
+model.add(Dense(24,activation='relu'))
+model.add(Dense(24,activation='relu'))
+model.add(Dense(actions,activation='linear'))
+
+agent=DQNAgent(
+  model=model,
+  memory=SequentialMemory(limit=50000,window_length=1),
+  nb_actions=actions,
+  nb_steps_warmup=10,
+  target_model_update=1e-2,
+  policy=BoltzmannQPolicy(),
+  
+  )
+
+agent.compile(optimizer=Adam(learning_rate =0.01),metrics=['mae'])
+agent.fit(env,nb_steps=100000,visualize=True,verbose=1)
+
+results=agent.test(env,nb_episodes=100,visualize=True)
+print(np.mean(results.history['episode_reward']))
 
 env.close()
+# for _ in range(1000):
+#     env.reset()
+#       # agent policy that uses the observation and info
+#     #action = env.action_space.sample()  # agent policy that uses the observation and info
+#     score=0
+#     terminated=False
+#     truncated=False
+#     while not terminated and not truncated:
+#         action = random.choice([0, 1])
+#         observation, reward, terminated, truncated, info = env.step(action)
+#         #print(observation, reward, terminated, truncated, info)
+#         score+=reward
+#         print(score)
+#         env.render()
+    
+    
+
+
+
